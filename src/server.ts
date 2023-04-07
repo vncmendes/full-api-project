2028a8674d437a78a26748ee3585a4aaa4be724e
Vinicius Mendes <viniciussmendes@gmail.com>
2023-04-07 00:14:42 +0000

    first code commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const server = fastify()
+
+server.get('/hello', () => {
+  return 'Hello World'
+})
+
+server.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server Running.');
+})
