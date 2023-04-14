824f72523b993dea7b31926ce739e9143ff00ff8
Vinicius Mendes <viniciussmendes@gmail.com>
2023-04-14 01:00:21 +0000

    trying to add info on table transactions, but the erro still happeens

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const server = fastify()
 
 server.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
 
 server
