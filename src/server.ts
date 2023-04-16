8ff7c3c881dbe63cf0150ad1bdfc810fd181b039
Vinicius Mendes <viniciussmendes@gmail.com>
2023-04-16 01:49:38 +0000

    adding zod on application, to control the enviroment variables and more.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const server = fastify()
 
@@ -18,7 +19,7 @@ server.get('/hello', async () => {
 
 server
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running.')
